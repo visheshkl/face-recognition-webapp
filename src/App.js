5916cf96c2cf3298ee81bfcfccf5ae687d90f95c
Vishesh Khandelwal <visheshkkl@gmail.com>
2021-01-29 02:54:42 +0530

    updated basic components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React,{Component} from 'react';
-import Navigation from './components/Navigation/navigation'
-import Logo from './components/Logo/logo'
+import Navigation from './components/Navigation/navigation';
+import Logo from './components/Logo/logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
 class App extends Component{
   render(){
     return(
       <div className='App'>
+        <Particles  className='particles'/>
         <Navigation/>
         <Logo/>
-        {/* 
+        <Rank/>
         <ImageLinkForm/>
+        {/*
         <FaceRecognition/> */}
       </div>
     );
--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import face from './face.png';
 import './logo.css'
 
 const Logo = () =>{
     return(
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
-                <div className="Tilt-inner"> 👽 </div>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+                <div className="Tilt-inner pa3"> <img alt='FaceX' style={{paddingTop:'5px'}}src={face}/> </div>
             </Tilt>
         </div>
     );
